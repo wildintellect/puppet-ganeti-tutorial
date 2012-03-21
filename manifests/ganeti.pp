03e33615c6f18d2f2a0df7a287b552f034fce5cd
Alex Mandel <madadh@puffin.(none)>
2012-03-21 14:32:20 -0700

    Switching to repo packages instead of source compiles

--- manifests/ganeti.pp
+++ manifests/ganeti.pp
@@ -1,36 +1,15 @@
 class ganeti::ganeti::install {
     require ganeti::params
 
-    $ganeti_version = "${ganeti::params::ganeti_version}"
+ #   $ganeti_version = "${ganeti::params::ganeti_version}"
 
-    file {
-        "/etc/init.d/ganeti":
-            ensure  => present,
-            require => Exec["install-ganeti"],
-            source  => "/root/src/ganeti-${ganeti_version}/doc/examples/ganeti.initd",
-            mode    => 755,
+    package {["ganeti2","ganeti-htools"]:
+            ensure  => installed,
     }
-
-#    ganeti::unpack {
-#        "ganeti":
-#            source      => "/root/src/ganeti-${ganeti_version}.tar.gz",
-#            cwd         => "/root/src/",
-#            creates     => "/root/src/ganeti-${ganeti_version}",
-#            require     => File["/root/src"];
-#    }
-
-#    exec {
-#        "install-ganeti":
-#            command => "/root/puppet/files/scripts/install-ganeti",
-#            cwd     => "/root/src/ganeti-${ganeti_version}",
-#            creates => "/usr/local/sbin/gnt-cluster",
-#            require => ganeti::Unpack["ganeti"];
-#    }
-
     service {
         "ganeti":
             enable      => true,
-            require     => File["/etc/init.d/ganeti"],
+            require     => Package["ganeti2"],
     }
 }
 
