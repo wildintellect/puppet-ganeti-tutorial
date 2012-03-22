d6769b0ba27f4f905ff03ff2f6032fee09897875
Alex <madadh@owl.(none)>
2012-03-22 01:16:22 -0700

    more cleanup

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,27 +1,21 @@
-# Ganeti Tutorial
+# Ganeti Installation
 
 class ganeti {
     require ganeti::params
-
     include ganeti::install_deps
     include ganeti::hosts
     include ganeti::drbd
 
     file {
-        "/root/.ssh":
-            ensure  => directory;
         "/var/lib/ganeti":
             ensure  => directory;
         "/var/lib/ganeti/rapi/":
             require => File["/var/lib/ganeti"],
             ensure  => directory;
-        "/root/puppet":
-            ensure  => link,
-            target  => "/etc/puppet/modules/ganeti";
-        "/var/lib/ganeti/rapi/users":
-            ensure  => "present",
-            mode    => 640,
-            require => File["/var/lib/ganeti/rapi/"],
-            source  => "${ganeti::params::files}/rapi-users";
+#        "/var/lib/ganeti/rapi/users":
+#            ensure  => "present",
+#            mode    => 640,
+#            require => File["/var/lib/ganeti/rapi/"],
+#            source  => "${ganeti::params::files}/rapi-users";
     }
 }
--- manifests/networking.pp
+++
@@ -1,6 +0,0 @@
-class ganeti::networking {
-    file { "/etc/network/interfaces":
-        ensure  => present,
-        content => template("ganeti/interfaces.erb"),
-    }
-}
