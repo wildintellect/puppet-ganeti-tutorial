d8d46c182446c1601a665affe355e98f58d7f9f4
Alex Mandel <tech@wildintellect.com>
2012-03-28 16:00:06 -0700

    moved hosts to a private module

--- manifests/hosts.pp
+++ manifests/hosts.pp
@@ -1,25 +1,6 @@
 class ganeti::hosts {
     host {
-        "ganeti.des.ucdavis.edu":
-            ensure          => present,
-            host_aliases    => "ganeti",
-            ip              => "169.237.167.61";
-        "ice1.des.ucdavis.edu":
-            ensure          => present,
-            host_aliases    => "ice1",
-            ip              => "169.237.167.49";
-        "ice2.des.ucdavis.edu":
-            ensure          => present,
-            host_aliases    => "ice2",
-            ip              => "169.237.167.50";
-	  "ice1drbd.des.ucdavis.edu":
-            ensure          => present,
-            host_aliases    => "ice1drbd",
-            ip              => "192.168.167.49";
-        "ice2drbd.des.ucdavis.edu":
-            ensure          => present,
-            host_aliases    => "ice2drbd",
-            ip              => "192.168.167.50";
+        #moved to different module
             }
 
 }
